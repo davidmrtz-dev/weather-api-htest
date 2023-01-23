a573b60ea0bc189235afc409fc53a3b8b3cf6072
David Mtz <davidelimg1@gmail.com>
2023-01-23 17:27:34 -0600

    [CHORE]: Implemented test for post

--- app/controllers/weathers_controller.rb
+++ app/controllers/weathers_controller.rb
@@ -1,2 +1,22 @@
 class WeathersController < ApplicationController
+  def create
+    weather = Weather.new(weather_params)
+     if weather.save!
+      render json: { data: weather }, status: :created
+     else
+      render json: { error: weather.errors.full_messages }, status: :unprocessable_entity
+     end
+  end
+
+  def weather_params
+    params.permit(
+      :description,
+      measurements: [],
+      locations_attributes: [
+        :description,
+        :lat,
+        :lng
+      ]
+    )
+  end
 end
